7a062c04ad13b71f63bcfa882d36423c5b3707e0
Perry Kivolowitz <perry@kivolowitz.com>
2021-01-17 15:28:40 -0600

    added some comments in an attempt to make Cephas happier.

--- language_concepts/do_while/do_while_asm.s
+++ language_concepts/do_while/do_while_asm.s
@@ -4,13 +4,15 @@
 main:	stp		x21, x30, [sp, -16]!
 		mov		w21, wzr
 
+		// Notice this drops right into the "action" - this is the "do"
+
 1:		adr		x0, fmt					// address of fmt goes in x0
 		mov		w1, w21					// value of i goes in w1
 		bl		printf					// printf(fmt, i)
 
 		add		w21, w21, 1
-		cmp		w21, 10
-		blt		1b
+		cmp		w21, 10					// here the "while"
+		blt		1b						// this is the closing brace on line 8
 
 		ldp		x21, x30, [sp], 16
 		mov		x0, xzr

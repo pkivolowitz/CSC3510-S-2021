3577834f59f651906d82bde5602c45e8b6c5407c
Perry Kivolowitz <perry@kivolowitz.com>
2021-01-17 15:29:13 -0600

    added some comments in an effort to cause Cephas joy.

--- language_concepts/break_continue/bc_asm.s
+++ language_concepts/break_continue/bc_asm.s
@@ -7,7 +7,7 @@
 	branching to the "top" of the loop is a continue.
 */
 
-main:	stp		x21, x30, [sp, -16]!
+main:	stp		x21, x30, [sp, -16]!	// save registers for later
 		mov		x21, -1
 
 1:		add		x21, x21, 1
@@ -15,13 +15,14 @@ main:	stp		x21, x30, [sp, -16]!
 		bne		2f
 		adr		x0, skip
 		bl		puts
-		b 		1b
+		b 		1b				// by branching back to the "top" this is a continue
 2:		cmp		x21, 10
-		blt		1b
-		adr		x0, brkg
+		blt		1b				// this is the closing brace on line 15 of the C code
+		adr		x0, brkg		// this path leads out of the loop so is the break
 		bl		puts
+		// The way the above was organized saved one or more instructions. Do you see it?
 
-		ldp		x21, x30, [sp], 16
+		ldp		x21, x30, [sp], 16	// restore registers in preparation of exiting
 		mov		x0, xzr
 		ret
 
